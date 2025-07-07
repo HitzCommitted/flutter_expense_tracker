a99d5ed4d7fffdb93829b9b10fc9dfb2900b5f6f
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-07-07 11:53:32 +0300

    working basic show modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(MaterialApp(home: Expenses()));
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -39,9 +39,25 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => Text('Modal bottom sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Track With Hitz'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The chart'),
